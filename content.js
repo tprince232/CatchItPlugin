e444515ae7dd49eac8058788e302fe53ecde4465
jem26 <jamiemaher@jamies-mbp.dhcp.nd.edu>
2017-12-03 17:43:05 -0500

    removed html tags from some fields

--- content.js
+++ content.js
@@ -6,10 +6,13 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
         // the web-page's DOM content as argument
         var data = {}
         data.title = document.getElementsByClassName("jobtitle")[0].innerHTML;
+        data.title = data.title.replace(/<(?:.|\n)*?>/gm, '');
         data.company = document.getElementsByClassName("company")[0].innerHTML;
+        data.company = data.company.replace(/<(?:.|\n)*?>/gm, '');
         data.location = document.getElementsByClassName("location")[0].innerHTML;
+        data.location = data.location.replace(/<(?:.|\n)*?>/gm, '');
         data.description = document.getElementById("job_summary").innerHTML;
-
+        // data.description = data.description.substring(0, 1000);
         sendResponse(data)
         //sendResponse(document.all[0].outerHTML);
     }
