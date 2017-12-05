e926b0506ce67823b66d3d7b5b297ac14f480676
jem26 <jamiemaher@jamies-mbp.dhcp.nd.edu>
2017-12-05 10:37:14 -0500

    formatting of city, state, and job title

--- content_gd.js
+++ content_gd.js
@@ -14,9 +14,17 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
             }
         }*/
         data.title = document.getElementsByClassName("breadcrumb ib ")[1].textContent;
+        data.title = data.title.replace(/<(?:.|\n)*?>/gm, '');
+        data.title = data.title.replace('>', '');
         data.company = document.getElementsByClassName("breadcrumb ib ")[4].textContent;
+        data.company = data.company.replace(/<(?:.|\n)*?>/gm, '');
         var state = document.getElementsByClassName("breadcrumb ib ")[2].textContent;
+        state = state.replace(/<(?:.|\n)*?>/gm, '');
+        state = state.replace('>', '');
         var city = document.getElementsByClassName("breadcrumb ib ")[3].textContent;
+        city = city.replace(/<(?:.|\n)*?>/gm, '');
+        city = city.replace('>', '');
+        city = city.slice(0, -2);
 
         data.location = city + ", " + state;
         data.description = document.getElementsByClassName("jobDescriptionContent desc")[0].innerHTML;
